a9f8bdb8c0da8c0a2110d048f54d6b6c75f877d8
MaoZao <salomao.3@gmail.com>
2021-08-20 15:48:24 -0300

    storage

--- infrastructure/aws/storage.tf
+++ infrastructure/aws/storage.tf
@@ -19,6 +19,9 @@ resource "aws_s3_bucket" "buckets" {
 resource "aws_s3_bucket" "bucket_functions" {
   bucket = "${local.prefix}-${var.bucket_functions}-${var.account}"
   acl    = "private"
+  key    = "emr-code/pyspark/job_spark.py"
+  source = "../etl/job_spark.py"
+  etag   = filemd5("../etl/job_spark.py")
 
   tags = local.common_tags
 
@@ -39,13 +42,4 @@ resource "null_resource" "fn_example_script" {
   provisioner "local-exec" {
     command = "zip -rj ../../functions/fn_example_script.zip ../../functions/fn_example_script"
   }
-}
-
-
-resource "aws_s3_bucket" "bucket_functions_emr" {
-  bucket = "${local.prefix}-${var.bucket_functions}-${var.account}"
-  key    = "emr-code/pyspark/job_spark.py"
-  acl    = "private"
-  source = "../etl/job_spark.py"
-  etag   = filemd5("../etl/job_spark.py")
 }
